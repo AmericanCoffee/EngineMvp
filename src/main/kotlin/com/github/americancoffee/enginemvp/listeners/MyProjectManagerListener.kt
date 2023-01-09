a8ca4dec7d407a25d44127e54c72b431f6a38c02
GitHub Action <action@github.com>
2023-01-09 06:49:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/americancoffee/enginemvp/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/americancoffee/enginemvp/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.americancoffee.enginemvp.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.americancoffee.enginemvp.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/americancoffee/enginemvp/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/americancoffee/enginemvp/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.americancoffee.enginemvp.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.americancoffee.enginemvp.MyBundle
 
 class MyApplicationService {
 
